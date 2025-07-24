ede45766593313b0431e647962e2fd71189892eb
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-07-24 15:40:49 +0100

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
+//1. Create an abstract class named `Phone`
+//1. the `Phone` class should have a phone number property
+//1. the `Phone` class should have a method named `call` that takes a single parameter of another phone object and
+// logs that phones number
+//1. create a class named `Landline` that inherits from `Phone` and has an `address` property
+//1. create a class named `Mobile` that inherits from `Phone` and has a method called `text` that takes another
+// `Mobile` as well as a string and logs the number as well as the message
