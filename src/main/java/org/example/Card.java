1e6ab8090f06d3ab68f35ffaf7cfebc0f969099d
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-07-24 20:51:12 +0100

    Fix(Card): Change Suit to use unicode chars

--- src/main/java/org/example/Card.java
+++ src/main/java/org/example/Card.java
@@ -9,22 +9,28 @@ package org.example;
 //Has an int value (2,3,4,5,6,7,8,9,10,11,12,13,14) FINISHED
 //- Each symbol is created as a part of an Enum FINISHED
 //- Each symbol has a string and int property of itself FINISHED
-//-
+//- Create a card constructor which will initialise the fields of suit and symbol FINISHED
+//- Create an array to push all cards to upon creation
+//- Create a for loop which will create all cards using both symbol and suit enums
 //Has a toString method that describes the class
 //- Create a toString method that will display each card class???
 
 public class Card {
 
     enum Suit {
-        SPADE,
-    CLUB,
-    HEART,
-    DIAMOND
+        SPADE('\u2660'), CLUB('\u2663'), HEART('\u2665'), DIAMOND('\u2666');
+
+        private final char value;
+        Suit(char suit) {
+            this.value = suit;
+        }
     }
 
     enum Symbol {
-        TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), JACK(10), Q(12), K(13), A(14);
+        TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8),
+        NINE(9), JACK(10), Q(12), K(13), A(14);
         private final int value;
+
         Symbol(int value) {
             this.value = value;
         }
@@ -32,8 +38,22 @@ public class Card {
         public int getValue() {
             return value;
         }
+    }
 
+    String symbolStr;
+    char suit;
+    int symbolInt;
 
+    public Card(Suit suit, Symbol symbol) {
+        this.symbolInt = symbol.value;
+//      This takes the enum given name and assigns it as the string
+        this.symbolStr = symbol.name();
+        this.suit = suit.value;
+    }
+
+    @Override
+    public String toString() {
+        return symbolStr + " of " + suit;
     }
 
 
