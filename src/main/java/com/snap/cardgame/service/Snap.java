d8405d098929e85ef90adee5a3fbde0e49f8bdaf
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-07-27 20:51:40 +0100

    Add(Snap): create players

--- src/main/java/com/snap/cardgame/service/Snap.java
+++ src/main/java/com/snap/cardgame/service/Snap.java
@@ -13,15 +13,17 @@ package com.snap.cardgame.service;
 //Add a timer so that when there is a snap opportunity, the player has 2 seconds to
 // submit the word “snap” in order to win. If they don’t type it in time, they lose.
 
+import com.snap.cardgame.model.Player;
+
 import java.util.Scanner;
 
+//- Create the startNewGame method, this must initialize game state and start a new turn FINISHED
 //- Create a user scanner input FINISHED
 //- Create an initial user greeting on game start FINISHED
-//- Create a handleUserInput method which will handle the flow of the game by user input
-//- Make the displayUserOptions method display the user options of  Start new game, Deal card,
-// Snap, End turn, How to play? Exit game, these should be associated with a number
-//- Depending on the round, this should display some or all on each user turn starting
-//- Create the startNewGame method, this must initialize game state and start a new turn
+//- Create a displayUserStartOptions which will display the starting options of a user FINISHED
+//- Create a handleUserStartOptions method which will handle the flow of the game by user input FINISHED
+//- Make the handleUserStartOptions method display the user options of  Start new game,
+// How to play?, Exit game, these should be associated with a number FINISHED
 //- The start new game should prompt the user to input how many players will join the game
 //- Create the startNewTurn method, this must alternate between players on each round and should
 // call on the displayUserOptions
@@ -34,6 +36,12 @@ import java.util.Scanner;
 //- Create a handleEndTurn method to end a players turn and start the next turn
 //- The handleEndTurn method should reset/set some game state vars like previousCard, currentCard,
 //- Create a displayInstructions method to print out some guidance to assist a user in playing
+//-
+//-Close the scanner
+//CHANGED-----------
+//- Make the displayUserOptions method display the user options of  Start new game, Deal card,
+// Snap, End turn, How to play? Exit game, these should be associated with a number
+//-
 //-
 //-
 public class Snap extends CardGame {
@@ -55,7 +63,7 @@ public class Snap extends CardGame {
 
     private void displayUserStartOptions() {
         System.out.println("\n" + "Welcome to Snap! Please input a number from the options below \n" +
-                "1) Start new game \n" +
+                "1) Ready to play! \n" +
                 "2) Show instructions? \n" +
                 "3) Exit game");
     }
@@ -66,7 +74,8 @@ public class Snap extends CardGame {
         switch (choice) {
             case "1":
                 System.out.println("------------------------");
-                System.out.println("Add user player functionality here");
+                displayPlayerSelect();
+                handlePlayerSelect();
                 break;
             case "2":
                 displayInstructions();
@@ -84,9 +93,6 @@ public class Snap extends CardGame {
                 displayUserStartOptions();
                 handleUserStartOptions();
         }
-
-
-        scanner.close();
     }
 
     private void displayInstructions() {
@@ -95,6 +101,32 @@ public class Snap extends CardGame {
                 "If a user calls snap in time they win! \n" +
                 "Calling snap is only dependent on the card values not the suits \n" +
                 "For example you can call snap on 2 of club and 2 of spades");
+    }
+
+    private void displayPlayerSelect() {
+        System.out.println("Please select how many players will be in the game up to a maximum of 2");
+    }
+
+    private void handlePlayerSelect() {
+        String choice = scanner.nextLine().trim();
+
+        switch (choice) {
+            case "1":
+                System.out.println("Add functionality to add 1 player");
+                new Player("Player 1");
+                break;
+            case "2":
+                System.out.println("------------------------");
+                new Player("Player 1");
+                new Player("Player 2");
+
+                break;
+            default:
+                System.out.println("------------------------");
+                System.out.println("Invalid choice, please input a correct option");
+                displayPlayerSelect();
+                handlePlayerSelect();
+        }
     }
 
     //- Make the displayUserOptions method display the user options of  Start new game, Deal card,
